71c0cdae9f30ecbd245c20bc7aa668557f9ebe97
Chris <mystik.vortex032@gmail.com>
2025-08-28 13:51:21 -0700

    Bullets and Movement Working

--- game.py
+++ game.py
@@ -1,33 +1,63 @@
 import pygame
 import sys
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
 
 class SpaceInvasionGame:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((640, 480))
-        # rest of settings
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Space Invasion")
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         while True:
-            self.check_events()
-            self.update_objects()
-            self.new_method()
+            self._check_events()
+            self.ship.update()
+            self.bullets.update()
+            self._update_screen()
             self.clock.tick(60)
 
-    def new_method(self):
-        self.update_screen()
-
-    def check_events(self):
+    def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_key_down(event)
+            elif event.type == pygame.KEYUP:
+                self._check_key_up(event)
 
-    def update_objects(self):
-        pass
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.ship.blitme()
+        pygame.display.flip()
 
-    def update_screen(self):
-        pass
+    def _check_key_down(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_key_up(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
 
 if __name__ == "__main__":
     game = SpaceInvasionGame()
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 600
+        self.screen_height = 400
+        self.bg_color = "BLACK"
+        self.bullet_speed = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (245,239,66)
+        self.bullets_allowed = 3
+        self.alien_speed = 1

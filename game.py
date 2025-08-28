aac37f8e13081cb4bb556c12995f4256ac4b94e5
Chris <mystik.vortex032@gmail.com>
2025-08-28 14:06:02 -0700

    fixed not being able to shoot after 3 shots

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ from ship import Ship
 from bullet import Bullet
 
 class SpaceInvasionGame:
+    #initializes game setup
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
@@ -14,14 +15,19 @@ class SpaceInvasionGame:
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
 
+    #runs the game and updates at 60 fps
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             self._update_screen()
             self.clock.tick(60)
 
+    #check for keyboard inputs
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,6 +37,7 @@ class SpaceInvasionGame:
             elif event.type == pygame.KEYUP:
                 self._check_key_up(event)
 
+    #update bullets and ship every frame
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         for bullet in self.bullets.sprites():
@@ -38,6 +45,7 @@ class SpaceInvasionGame:
         self.ship.blitme()
         pygame.display.flip()
 
+    #check which key is pressed and associate that to an event
     def _check_key_down(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
@@ -48,15 +56,18 @@ class SpaceInvasionGame:
         elif event.key == pygame.K_q:
             sys.exit()
 
+    #check when a key is released to stop player movement
     def _check_key_up(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
     
+    #creates a new bullet and adds it to the bullets group
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < 3: #no more than 3 bullets at a time
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
 if __name__ == "__main__":
